f6c89c72542b5a8ff809e11f6d0a905ff09c2446
satya M <satya2maity@gmail.com>
2023-01-28 20:59:05 +0530

    added new

--- simpleNode/index.js
+++ simpleNode/index.js
@@ -1,5 +1,13 @@
 const fs=require('fs')
 
-fs.writeFileSync('read.txt',"Hi I am Satyaranjan Maity")
+// fs.writeFileSync('read.txt',"Hi I am Satyaranjan Maity")
 
-// fs.writeFileSync('read.txt',"Hi I am Santosh Yadav")
+// fs.writeFileSync('read.txt',"Hi I am Santosh Yadav\n")
+
+// fs.appendFileSync('read.txt',"How Are You all\n")
+
+const read_data=fs.readFileSync('read.txt')
+
+org_data=read_data.toString();
+
+console.log(org_data);

ceaff4df396dc47f6af6d50e444033f63ec718bd
satya M <satya2maity@gmail.com>
2023-01-29 21:41:30 +0530

    added new

--- AsyncSystem/index.js
+++ AsyncSystem/index.js
@@ -1,3 +1,22 @@
 const fs=require('fs')
 
-fs.writeFile
+// fs.writeFile('note.txt',"Hi This is Asynchronous Example",(err,data) => {
+//     console.log(data)
+// })
+
+
+//For Reading a Asynchronous files 
+
+// fs.readFile('note.txt',"UTF-8",(err,data) => {
+//     console.log(data)
+// })
+
+
+//FOR APPENDING Files 
+
+fs.appendFile("note.txt","Data appended successfully",(err,data) =>{
+    console.log(data);
+    
+})
+
+

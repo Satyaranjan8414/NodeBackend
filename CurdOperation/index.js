e78b424d6ed7d59d4ce12150fd456647f9d96c60
satya M <satya2maity@gmail.com>
2023-02-01 04:42:14 +0530

    added new

--- CurdOperation/index.js
+++ CurdOperation/index.js
@@ -7,6 +7,12 @@ fs.mkdir("nameOfTheFolder",(err)=>{
     console.log("Folder created");
 })
 
+//To Remove a Folder From Exiting File We have Write Command
+fs.rmdir("nameOfTheFolder",(err)=>{
+    console.log("Folder removed");
+    
+});
+
 
 //To Wite the file we have to Wite This Commnad 
 

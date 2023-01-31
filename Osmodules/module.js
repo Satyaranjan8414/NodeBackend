cbe1cdfdae5e7e9ef17b394e91da9458b4a09906
satya M <satya2maity@gmail.com>
2023-02-01 04:53:41 +0530

    added new

---
+++ Osmodules/module.js
@@ -0,0 +1,14 @@
+const os=require("os")
+
+//arch Is Gives the architecture 
+
+// console.log(os.arch());
+
+//How much space is available
+
+const freeSpace=os.freemem();
+
+//It give us in bits of free
+// console.log(freeSpace);
+
+//To GB of free

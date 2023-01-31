44cff3817c9859862e4e548880d2052c93358b2a
satya M <satya2maity@gmail.com>
2023-02-01 04:32:31 +0530

    added new

--- CurdOperation/index.js
+++ CurdOperation/index.js
@@ -2,16 +2,16 @@ const fs=require('fs');
 
 //To Wite the file we have to Wite This Commnad 
 
-// fs.writeFileSync('bio.txt',"Hi This is Curd Files")
+fs.writeFileSync('bio.txt',"Hi This is Curd Files")
 
 //To Apppend without Removing Existing File then we have go through this command 
 
-// fs.appendFileSync('bio.txt',"I ma satyaranjan maity")
+fs.appendFileSync('bio.txt',"I ma satyaranjan maity")
 
 // To Rename the file
 
-// fs.renameSync('bio.txt','mybio.txt')
+fs.renameSync('bio.txt','mybio.txt')
 
 //To Delete the file
 
-// fs.unlinkSync('mybio.txt');
+fs.unlinkSync('mybio.txt');

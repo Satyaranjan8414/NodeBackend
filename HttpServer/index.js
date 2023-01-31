9875f047ef06bc3d7163eb3bb290a64b4bb3d154
satya M <satya2maity@gmail.com>
2023-02-01 04:44:26 +0530

    added

--- HttpServer/index.js
+++ HttpServer/index.js
@@ -1,19 +1,19 @@
 //Build Your won Server
 
-const http=require('http')
- const server =http.createServer((req,res)=>{
-    if(req.url=='/'){
-        res.end("<h1>Hello From landing Page</h1>")
-    }else if(req.url=='/about'){
-        res.end("<h1>Hello From about Page</h1>")
-    }else if(req.url=='/result'){
-        res.end("<h1>Hello From result Page</h1>")
-    }else {
-        res.writeHead(404,{"Content-Type":"text.html"})
-        res.end("<h1>404 Page Not Found Error</h1>")
-    }
- })
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("<h1>Hello From landing Page</h1>");
+  } else if (req.url == "/about") {
+    res.end("<h1>Hello From about Page</h1>");
+  } else if (req.url == "/result") {
+    res.end("<h1>Hello From result Page</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text.html" });
+    res.end("<h1>404 Page Not Found Error</h1>");
+  }
+});
 
- server.listen(8080,'127.0.0.1',()=>{
-    console.log("help Please")
- })
+server.listen(8080, "127.0.0.1", () => {
+  console.log("help Please");
+});
